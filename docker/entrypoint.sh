fc3f20ee2ad5ae524fce93cc04ddb8666e6e4860
Daniel Bast <2790401+dbast@users.noreply.github.com>
2020-04-20 14:27:53 +0200

    Extend Docker entrypoint.sh to support installing extra-system-packages
    The docker container currently contains the bare minimum list of packages
    to get an image built. By adding a parameter to the entrypoint.sh script
    more packages can be installed along the way in a flexible way, without
    the need to extend the provided container.
    
    E.g. useable to add bmap-tools or zstd.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,22 @@ bash /register --reset -p yes >/dev/null 2>&1
 
 PACKER=/bin/packer
 
-echo running "${PACKER}"
+declare -a EXTRA_SYSTEM_PACKAGES=()
+for arg do
+    shift
+    if [[ "${arg}" == -extra-system-packages=* ]]; then
+        IFS=',' read -r -a EXTRA_SYSTEM_PACKAGES <<< "${arg//-extra-system-packages=}"
+        continue
+    fi
+    set -- "$@" "${arg}"
+done
+
+if [ -n "${EXTRA_SYSTEM_PACKAGES[0]}" ]; then
+    echo "Installing extra system packages: ${EXTRA_SYSTEM_PACKAGES[*]}"
+    apt-get update
+    apt-get install -y --no-install-recommends "${EXTRA_SYSTEM_PACKAGES[@]}"
+fi
+
+echo running "${PACKER}" "${@}"
 
 exec "${PACKER}" "${@}"
