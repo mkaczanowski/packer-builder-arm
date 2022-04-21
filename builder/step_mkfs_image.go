8cf0f84ba0204cecbd6231ccc1fd1cd26fab2f4a
nibbler <12103270+v4n6@users.noreply.github.com>
2022-04-21 23:11:52 +0200

    changed mkfs message; cleaned up code

--- builder/step_mkfs_image.go
+++ builder/step_mkfs_image.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os/exec"
+	"strings"
 
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
 	"github.com/hashicorp/packer-plugin-sdk/packer"
@@ -15,18 +16,17 @@ type StepMkfsImage struct {
 }
 
 // Run the step
-func (s *StepMkfsImage) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {
+func (s *StepMkfsImage) Run(_ context.Context, state multistep.StateBag) multistep.StepAction {
 	ui := state.Get("ui").(packer.Ui)
 	config := state.Get("config").(*Config)
 	loopDevice := state.Get(s.FromKey).(string)
 
-	ui.Message("running mkfs")
 	for i, partition := range config.ImageConfig.ImagePartitions {
-		out, err := exec.Command(
-			fmt.Sprintf("mkfs.%s", partition.Filesystem),
-			append( partition.FilesystemMakeOptions,
-				fmt.Sprintf("%sp%d", loopDevice, i+1))...
-			).CombinedOutput()
+		cmd := fmt.Sprintf("mkfs.%s", partition.Filesystem)
+		args := append(partition.FilesystemMakeOptions, fmt.Sprintf("%sp%d", loopDevice, i+1))
+
+		ui.Message(fmt.Sprintf("creating partition #%d: mkfs.%s %s", i+1, cmd, strings.Join(args, " ")))
+		out, err := exec.Command(cmd, args...).CombinedOutput()
 
 		if err != nil {
 			ui.Error(fmt.Sprintf("error mkfs %v: %s", err, string(out)))
@@ -38,4 +38,4 @@ func (s *StepMkfsImage) Run(ctx context.Context, state multistep.StateBag) multi
 }
 
 // Cleanup after step execution
-func (s *StepMkfsImage) Cleanup(state multistep.StateBag) {}
+func (s *StepMkfsImage) Cleanup(_ multistep.StateBag) {}
