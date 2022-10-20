b06a7db1ad62ecd4bedafae51dc00dc38991e8e9
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-10-20 20:06:18 +0200

    Rename to packer-plugin-arm

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/mkaczanowski/packer-builder-arm/builder"
+	"github.com/mkaczanowski/packer-plugin-arm/builder"
 	"os"
 )
 
 func main() {
 	pps := plugin.NewSet()
-	pps.RegisterBuilder(plugin.DEFAULT_NAME, builder.NewBuilder())
+	pps.RegisterBuilder(plugin.DEFAULT_NAME, new(builder.Builder))
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
