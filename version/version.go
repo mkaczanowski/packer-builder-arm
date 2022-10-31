0383bf69de73e587a2b2a1aa3d03760ebea6d149
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-10-31 16:30:14 +0100

    Final version

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ var (
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
 	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
+	VersionPrerelease = ""
 
 	// PluginVersion is used by the plugin set to allow Packer to recognize
 	// what version this plugin is.
