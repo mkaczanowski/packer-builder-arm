5470d7c3079531bd46aab472725caffadd0444f3
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-10-31 13:24:39 +0100

    Add plugin-check and version

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 	"github.com/mkaczanowski/packer-plugin-arm/builder"
+	"github.com/mkaczanowski/packer-plugin-arm/version"
 	"os"
 )
 
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterBuilder(plugin.DEFAULT_NAME, new(builder.Builder))
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
