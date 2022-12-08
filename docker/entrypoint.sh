a9af016fb59899f2876f572fd5aa956fd0ec946b
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-12-08 16:37:53 +0100

    Fix qemu container integration

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,33 @@ set -o errtrace -o nounset -o pipefail -o errexit
 
 echo "uname -a: $(uname -a)"
 
-/usr/bin/binfmt --install all
-
 PACKER=/bin/packer
+
+setup_qemu() {
+    # See also:
+    #   * https://github.com/qemu/qemu/blob/master/scripts/qemu-binfmt-conf.sh
+    #   * https://github.com/tonistiigi/binfmt/blob/master/cmd/binfmt/main.go
+    #   * https://docs.kernel.org/admin-guide/binfmt-misc.html
+
+    # mount binfmt_misc to be able to register qemu binaries
+    mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
+
+    # reset
+    find /proc/sys/fs/binfmt_misc -type f -name 'qemu-*' -exec sh -c 'echo -1 > "$1"' shell {} \;
+
+    uname_m="$(uname -m)"
+    if [ "$uname_m" != "aarch64" ]; then
+        echo "Register qemu-aarch64"
+        echo ":qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-aarch64-static:F" > /proc/sys/fs/binfmt_misc/register
+    fi
+    echo "Register qemu-arm"
+    echo ":qemu-arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:F" > /proc/sys/fs/binfmt_misc/register
+}
+
+do_qemu_setup=${SETUP_QEMU:-true}
+if [ "$do_qemu_setup" = true ]; then
+    setup_qemu
+fi
 
 declare -a EXTRA_SYSTEM_PACKAGES=()
 for arg do
